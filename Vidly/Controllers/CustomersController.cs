a5d9d2c23adb27f78b3ebf067607e7118f668a29
Elmasry <Elmasry@ELMASRY-PC>
2017-07-25 17:22:43 +0200

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly List<Customer> _customers = new List<Customer>
+        {
+            new Customer {Id = 1, Name = "John Doe"},
+            new Customer {Id = 2, Name = "Jean Doe"}
+        };
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new CustomersViewModel
+            {
+                Customers = _customers
+            };
+            return View(customers);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            var customer = _customers.FirstOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound($"Customer with id {id} is not found!");
+            return View(customer);
+        }
+    }
+}
